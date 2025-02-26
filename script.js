e405449ee10d2b3ce71853e81f3871db50cdce5f
Irfan Ansari <123730480+irfanAnsari-2023@users.noreply.github.com>
2025-02-26 12:52:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
 //complete this code
-class Animal {}
+class Animal {
+	constructor(species:string){
+		this.species = species;
+	}
 
-class Dog extends Animal {}
+	get species():string{
+		return this.species;
+	}
 
-class Cat extends Animal {}
+	makeSound():void{
+		console.log(`The ${this._species} makes a sound`);
+	}
+}
+
+class Dog extends Animal {
+	constructor(species: string) {
+    super(species); 
+  }
+	purr():void{
+		console.log("purr");
+	}
+}
+
+class Cat extends Animal {
+
+	constructor(species: string) {
+    super(species); 
+  }
+	bark():void{
+		console.log("woof");
+	}
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
